4feee14066c83a491e0d126d5b5ccf1ebb93f3d2
Wajiddat18c <wajid0003@stud.kea.dk>
2020-04-22 11:54:27 +0200

    first commit

---
+++ src/main/java/padc/dat18c/renoblvd/RenoblvdApplication.java
@@ -0,0 +1,13 @@
+package padc.dat18c.renoblvd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenoblvdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenoblvdApplication.class, args);
+    }
+
+}
