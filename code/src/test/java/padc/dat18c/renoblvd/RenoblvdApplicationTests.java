444deea3b2127b8a58ce8e67c8e8c5416b136cf1
Wajiddat18c <wajid0003@stud.kea.dk>
2020-04-23 10:04:14 +0200

    code

---
+++ code/src/test/java/padc/dat18c/renoblvd/RenoblvdApplicationTests.java
@@ -0,0 +1,13 @@
+package padc.dat18c.renoblvd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenoblvdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
