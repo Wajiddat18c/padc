e0bd42a4df8e1eb00f2ae7d2d03c066c2dc6de32
Wajiddat18c <wajid0003@stud.kea.dk>
2020-04-25 23:37:54 +0200

    added CRUD to newsletter

--- code/src/main/java/padc/dat18c/renoblvd/controller/MainController.java
+++ code/src/main/java/padc/dat18c/renoblvd/controller/MainController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import padc.dat18c.renoblvd.model.Newsletter;
 import padc.dat18c.renoblvd.service.NewsletterService;
 
 
@@ -36,5 +39,29 @@ public class MainController {
         return "redirect:/news";
     }
 
+
+    @GetMapping("/createNewsletter")
+    public String  createTeachers(){
+        return "createNewsletter";
+    }
+
+    @PostMapping("createNewsletter")
+    public String createTeachers(@ModelAttribute Newsletter newsletter)
+    {
+        newsletterService.create(newsletter);
+        return "redirect:/news";
+    }
+
+
+    @GetMapping("updateNewsletter/{idNews}")
+    public String updateNewsletter(@PathVariable("idNews") int idNews, Model model){
+        model.addAttribute("newsletter", newsletterService.findById(idNews));
+        return "updateNewsletter";
+    }
+    @PostMapping("/updateNewsletter")
+    public String updateTeacher(@ModelAttribute Newsletter newsletter){
+        newsletterService.update(newsletter);
+        return "redirect:/news";
+    }
 
 }
