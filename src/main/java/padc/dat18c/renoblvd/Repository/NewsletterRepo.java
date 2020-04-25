e639991617abedddfdac59d1bed6981e64493769
Wajiddat18c <wajid0003@stud.kea.dk>
2020-04-25 15:26:23 +0200

    .

--- src/main/java/padc/dat18c/renoblvd/Repository/NewsletterRepo.java
+++
@@ -1,9 +0,0 @@
-package padc.dat18c.renoblvd.Repository;
-
-
-import padc.dat18c.renoblvd.model.Newsletter;
-import org.springframework.data.repository.CrudRepository;
-
-public interface NewsletterRepo extends CrudRepository<Newsletter, Integer > {
-
-}
